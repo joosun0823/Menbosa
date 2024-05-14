510ed74f4067cd2924028f831d586a8b22b21894
joosun0823 <sun10220466@gmail.com>
2024-05-14 13:42:44 +0900

    240514 시설후기 댓글창 고정

--- protector/protectorCommunity/protectorCommunity-communicate/protectorCommunity-communicateDetails/protectorCommunity-communicateDetails.js
+++ protector/protectorCommunity/protectorCommunity-communicate/protectorCommunity-communicateDetails/protectorCommunity-communicateDetails.js
@@ -24,34 +24,3 @@ for(let i = 0; i < commentMenu.length; i ++)  {
 
 }
 
-
-// const commentBar = document.querySelector(".protectorCommunity-communicateDetails-commentsInput");
-// const footer = document.querySelector("footer");
-
-// window.addEventListener("scroll", function() {
-//   let location = this.window.pageYOffset;
-//   console.log(location);
-
-
-//   // if(location < this.document.) {
-//   //   commentBar.style.position = "fixed";
-//   // } else{
-//   //   commentBar.style.position = "relative";
-//   // }
-
-//   if(location < footer.getBoundingClientRect().y){
-//     commentBar.style.position = "fixed";
-//     commentBar.style.bottom = "200px";
-    
-//   } else{
-
-//     commentBar.style.position = "relative";
-//   }
-
-// })
-
-
-
-
-// console.log(footer.getBoundingClientRect().y);
-// console.log(document.body.clientHeight);
