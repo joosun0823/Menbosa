590a9c55d5e3c10b60cb14bf5873e1d6f7351477
HyoJun00 <y23863707@gmail.com>
2024-05-29 00:23:04 +0900

    240529

---
+++ Menbosa/src/main/java/com/example/menbosa/controller/protector/user/UserController.java
@@ -0,0 +1,44 @@
+package com.example.menbosa.controller.protector.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/alheum/user")
+public class UserController {
+    @GetMapping()
+    public String login() {
+        return "/protector/protectorLogin";
+    }
+
+    @GetMapping("/findPassword")
+    public String findPassword() {
+        return "/protector/protectorPassword-find";
+    }
+
+    @GetMapping("/join")
+    public String join() {
+        return "/protector/protectorJoin";
+    }
+
+    @GetMapping("/map/hospital")
+    public String mapHospital() {
+        return "/protector/protectorRecommend-hospital";
+    }
+
+    @GetMapping("/map/sanatorium")
+    public String mapSanatorium() {
+        return "/protector/protectorRecommend-sanatorium";
+    }
+
+    @GetMapping("/map/welfare")
+    public String mapWelfare(){
+        return "/protector/protectorRecomment-welfare";
+    }
+
+    @GetMapping("/intro")
+    public String intro() {
+        return "/protector/protectorIntroduce";
+    }
+}
