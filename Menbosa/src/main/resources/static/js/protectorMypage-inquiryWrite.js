9b8b508a2b66a53586ccccb83f451395fd7193bf
HyoJun00 <157595885+HyoJun00@users.noreply.github.com>
2024-05-31 20:29:24 +0900

    Delete Menbosa directory

--- Menbosa/src/main/resources/static/js/protectorMypage-inquiryWrite.js
+++
@@ -1,6 +0,0 @@
-const inputFile = document.querySelector("input[type='file']");
-
-function deleteFile(e) {
-  console.log(inputFile);
-  inputFile.value = '';
-}
--- Menbosa/src/main/resources/static/js/seniorMypage.js
+++
@@ -1,7 +0,0 @@
-let secessionButton = document.querySelector(".secessionButton");
-
-secessionButton.addEventListener("click", function(){
-    if(confirm("탈퇴 하시겠습니까?")){
-      alert("탈퇴되었습니다.");
-    }
-});
